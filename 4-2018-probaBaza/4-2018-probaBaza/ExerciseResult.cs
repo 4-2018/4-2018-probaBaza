eec11367e2b846b1bd920fbeb1003274d65d7a7a
jocololika1999@gmail.com <jocololika1999@gmail.com>
2020-11-08 11:07:09 +0100

    Kreirana model klasa 	new file:   4-2018-probaBaza/4-2018-probaBaza/ExerciseResult.cs

---
+++ 4-2018-probaBaza/4-2018-probaBaza/ExerciseResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4_2018_probaBaza
+{
+    class ExerciseResult
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+        public int Points  { get; set; }
+
+        public ExerciseResult(int Id, string StudentName, string StudentIndex, int Points)
+        {
+            this.Id = Id;
+            this.StudentName = StudentName;
+            this.StudentIndex = StudentIndex;
+            this.Points = Points;
+        }
+    }
+}
